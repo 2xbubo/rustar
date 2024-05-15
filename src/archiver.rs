4e2356e059bb9d9908d38cbdf782b258602caa25
Bubobbubo <iamfilin@gmail.com>
2024-05-16 00:37:58 +0300

    wip fix files paths to relatives in archive file table

--- src/archiver.rs
+++ src/archiver.rs
@@ -29,8 +29,8 @@ fn read_file(file_path: &Path) -> Result<Vec<u8>, String> {
     }
 }
 
-pub fn archive_folder(folder_path: &str, file_path: &str) -> Result<(), String> {
-    let file = match fs::File::create(file_path) {
+pub fn archive_folder(folder_path: &str, archive_path: &str) -> Result<(), String> {
+    let achive_file = match fs::File::create(archive_path) {
         Ok(file) => file,
         Err(_) => return Err("Can't create archive file".to_string())
     };
@@ -39,19 +39,24 @@ pub fn archive_folder(folder_path: &str, file_path: &str) -> Result<(), String> 
     get_files_in_folder(Path::new(folder_path), &mut files_paths)?;
     
     let mut file_table = file_helpers::FileTable::new();
-    let mut writer = file_helpers::FileWriter::new(file);
+    let mut achive_writer = file_helpers::FileWriter::new(achive_file);
 
-    writer.write_data(&0u64.to_ne_bytes())?;
+    achive_writer.write_data(&0u64.to_ne_bytes())?;
 
     for file_path in files_paths {
+        let file_path = match fs::canonicalize(file_path.as_path()) {
+            Ok(file_path) => file_path,
+            Err(_) => return Err(format!("Can't read file {}", file_path.as_path().display().to_string()))
+        };
+
         let file_data = read_file(&file_path)?;
 
-        file_table.add_file(file_path, file_data.len() as u64, writer.get_offser());
+        file_table.add_file(file_path, file_data.len() as u64, achive_writer.get_offser());
 
-        writer.write_data(&file_data)?;
+        achive_writer.write_data(&file_data)?;
     }
 
-    file_table.write_to_file(&mut writer)?;
+    file_table.write_to_file(&mut achive_writer)?;
 
     Ok(())
 }
